0b672caf810a093c2ea834d5b8557a5bd7d379b7
Marcin Kacprzak <martin.kacprzak@gmail.com>
2013-10-19 14:40:28 +0200

    cw06-05 and 06

---
+++ cw06/cw06-05.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# cw06-05
+
+for x in range(1, 22):
+    print('{} * {} = {}'.format(x, x, x*x))
---
+++ cw06/cw06-06.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# cw06-06
+
+for x in range(1, 11):
+    for y in range(1, 11): 
+        print('{0:4d}'.format(x*y), end='')
+    print()
